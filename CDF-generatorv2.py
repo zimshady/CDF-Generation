6ba35c6cba0237eb7f75fbab50a9e89221e27558
Ton <zimshady6@gmail.com>
2016-11-15 22:16:20 +0000

    Updated legend

--- CDF-generatorv2.py
+++ CDF-generatorv2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data_req = pd.read_table("E:\GoogleDrive\PhD\Data\Detrital mica\All_onshore_DM_ages.csv", sep=",")
+data_req = pd.read_table("C:\Google Drive\PhD\Data\Detrital mica\Onshore_DM_ages_inc_Ennis2015_exc_Kiltorcan.csv", sep=",")
 #sort values per column
 arr = data_req.values
 arr.sort(axis=0)
@@ -15,5 +15,5 @@ data_req = pd.DataFrame(arr, index=data_req.index, columns=data_req.columns)
 for col in data_req.columns:
     y = np.linspace(0.,1., len(data_req[col].dropna()))
     plt.plot(data_req[col].dropna(), y, drawstyle='steps',label=str(col))
-plt.legend(loc='lower right')
+plt.legend(loc='lower right', fontsize = 12)
 plt.show()
